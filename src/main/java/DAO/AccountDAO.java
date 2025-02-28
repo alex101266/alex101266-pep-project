295e3ff8b1d84283bd57f4b35f975012660e5b6b
Alex Modzelewski <alex.modz219@gmail.com>
2025-02-28 18:55:58 +0000

    Completed AccountService.java

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 
 public class AccountDAO {
-    
+
+
 }
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -7,5 +7,49 @@ import java.util.List;
 
 
 public class AccountService {
-    
+    private AccountDAO accountDAO;
+
+    /**
+     * No argument constructor for creating an AccountService with a new AccountDAO
+     */
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    /**
+     * Constructor for AccountService when AccountDAO is known
+     * @param accountDAO to be used to create this AccountService
+     */
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    /**
+     * Verify that username isn't blank or already used and that password is at least 4 characters long.
+     * If valid, use accountDAO to persist the account.
+     * @param account
+     * @return
+     */
+    public Account registerAccount(Account account){
+        String username = account.getUsername().strip();
+        String password = account.getPassword().strip();
+        if( (username.length()>0) && (password.length()>3) && (accountDAO.getAccountByUsername(username)==null) ){
+            Account returnedAccount = accountDAO.insertAccount(account);
+            return returnedAccount;
+        } else{
+            return null;
+        }
+    }
+
+    /**
+     * Authorize login if account credentials are authenticated by accountDAO
+     * @param account to be authenticated
+     * @return account if credentials are valid for login
+     */
+    public Account authorizeLogin(Account account){
+        Account returnedAccount = accountDAO.authenticateLogin(account);
+        return returnedAccount;
+    }
+
+
 }
